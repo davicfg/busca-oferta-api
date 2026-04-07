32d1f1b1013495d55d1879d8bef4ee9749256413
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2026-04-07 00:51:36 -0300

    Creates initial versions of controllers and services

---
+++ busca-oferta-api/src/main/java/alexandre_davi_miguel/busca_oferta_api/service/MonitoramentoService.java
@@ -0,0 +1,7 @@
+package alexandre_davi_miguel.busca_oferta_api.service;
+
+public class MonitoramentoService {
+
+	// To Do:
+	// Lógica de pegar a URL, "decodifica-la" e encaminhar para salvar Produto no banco
+}
