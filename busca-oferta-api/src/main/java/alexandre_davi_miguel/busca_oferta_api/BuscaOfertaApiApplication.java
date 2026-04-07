24bb6bf956e69ec1a4c0fc3246dcefe3eff5a6bb
unknown <alexandrefilho27@gmail.com>
2026-04-06 22:45:46 -0300

    Inicialização do projeto

---
+++ busca-oferta-api/src/main/java/alexandre_davi_miguel/busca_oferta_api/BuscaOfertaApiApplication.java
@@ -0,0 +1,13 @@
+package alexandre_davi_miguel.busca_oferta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaOfertaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaOfertaApiApplication.class, args);
+	}
+
+}
---
+++ busca-oferta-api/src/test/java/alexandre_davi_miguel/busca_oferta_api/BuscaOfertaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package alexandre_davi_miguel.busca_oferta_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaOfertaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
