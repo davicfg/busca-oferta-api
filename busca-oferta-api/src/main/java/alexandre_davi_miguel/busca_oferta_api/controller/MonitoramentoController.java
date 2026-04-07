4cc93e2a7206726a9aba1e477e76488639fc83c3
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2026-04-07 00:51:36 -0300

    Creates initial versions of controllers and services

---
+++ busca-oferta-api/src/main/java/alexandre_davi_miguel/busca_oferta_api/controller/MonitoramentoController.java
@@ -0,0 +1,9 @@
+package alexandre_davi_miguel.busca_oferta_api.controller;
+
+public class MonitoramentoController {
+
+	// To Do:
+	// Endpoint para receber URL a ser monitorada
+	// Endpoint para listar produtos monitorados
+	// Endpoint para histórico de preco
+}
---
+++ busca-oferta-api/src/main/java/alexandre_davi_miguel/busca_oferta_api/controller/UsuarioController.java
@@ -0,0 +1,8 @@
+package alexandre_davi_miguel.busca_oferta_api.controller;
+
+public class UsuarioController {
+
+	// To Do:
+	// Endpoints para CRUD de usuário
+	// Endpoint para login de usuário
+}
