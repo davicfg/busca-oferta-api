c73ed29a11df186f83a434f15a1699e659325188
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2026-04-08 03:19:03 -0300

    Adds entities to the model package

---
+++ busca-oferta-api/src/main/java/alexandre_davi_miguel/busca_oferta_api/model/Produto.java
@@ -0,0 +1,25 @@
+package alexandre_davi_miguel.busca_oferta_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "produtos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String categoria;
+
+}
