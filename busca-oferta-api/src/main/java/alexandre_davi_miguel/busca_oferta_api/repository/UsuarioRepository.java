196b61b7a367e14836e8912ade4a87ef4d90eb2e
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2026-04-08 04:59:47 -0300

    Creates classes linked to 'Usuario'

---
+++ busca-oferta-api/src/main/java/alexandre_davi_miguel/busca_oferta_api/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package alexandre_davi_miguel.busca_oferta_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import alexandre_davi_miguel.busca_oferta_api.model.Usuario;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+    
+    boolean existsByEmail(String email);
+}
