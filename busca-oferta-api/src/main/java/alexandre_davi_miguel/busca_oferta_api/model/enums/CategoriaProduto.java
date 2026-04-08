5d14684867e9fca804a3a34125aa31911456e25c
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2026-04-08 03:52:47 -0300

    Creates an Enum for 'CategoriaProduto'

---
+++ busca-oferta-api/src/main/java/alexandre_davi_miguel/busca_oferta_api/model/enums/CategoriaProduto.java
@@ -0,0 +1,11 @@
+package alexandre_davi_miguel.busca_oferta_api.model.enums;
+
+public enum CategoriaProduto {
+    ALIMENTOS,
+    BEBIDAS,
+    LIMPEZA,
+    HIGIENE_PESSOAL,
+    ELETRODOMESTICOS,
+    ELETRONICOS,
+    OUTROS
+}
