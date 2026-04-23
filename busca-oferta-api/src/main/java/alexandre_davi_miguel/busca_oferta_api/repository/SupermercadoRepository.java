9cc1bc536a48cd4229823d09cfb21eba846220bd
davicfg <davicfg@gmail.com>
2026-04-22 23:28:24 -0300

    feat: implement Preco entity with validation, add Supermercado entity, and update related DTOs and services

---
+++ busca-oferta-api/src/main/java/alexandre_davi_miguel/busca_oferta_api/repository/SupermercadoRepository.java
@@ -0,0 +1,7 @@
+package alexandre_davi_miguel.busca_oferta_api.repository;
+
+import alexandre_davi_miguel.busca_oferta_api.model.Supermercado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupermercadoRepository extends JpaRepository<Supermercado, Long> {
+}
