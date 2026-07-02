aec5a89b77d361529890c26b769c962d3d26546a
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2026-07-02 01:13:30 -0300

    Criando instancia 2 do framework

---
+++ catalogo-pecas-api/src/main/java/alexandre_davi_miguel/catalogo_pecas_api/CatalogoPecasApiApplication.java
@@ -0,0 +1,13 @@
+package alexandre_davi_miguel.catalogo_pecas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoPecasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoPecasApiApplication.class, args);
+	}
+
+}
